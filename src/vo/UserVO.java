4dbad71335813c033a1d496a319423abbe77f295
TJ <TJ@DESKTOP-G4SIDO2>
2020-06-03 12:04:34 +0900

    userVo update

--- src/vo/UserVO.java
+++ src/vo/UserVO.java
@@ -1,27 +1,73 @@
 package vo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class UserVO extends Throwable {
+	
 	private int no;
 	private String id;
-	private String pw;
+	private String password;
 	private String name;
 	private int money;
 	private boolean admin;
 	private int win;
 	private int lose;
 	private boolean online;
+	private BufferedReader br;
+	private PrintWriter pw;
+	private Socket socket;
+	
+	public Socket getSocket() {
+		return socket;
+	}
+
+	public void setSocket(Socket socket) {
+		try {
+			br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		this.socket = socket;
+	}
 
 	public UserVO(int no, String id, String pw, String name, int money, boolean admin, int win, int lose,
 			boolean online) {
 		this.no = no;
 		this.id = id;
-		this.pw = pw;
+		this.password = pw;
 		this.name = name;
 		this.money = money;
 		this.admin = admin;
 		this.win = win;
 		this.lose = lose;
 		this.online = online;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public BufferedReader getBr() {
+		return br;
+	}
+
+	public void setBr(BufferedReader br) {
+		this.br = br;
+	}
+
+	public void setPw(PrintWriter pw) {
+		this.pw = pw;
 	}
 
 	public int getNo() {
@@ -41,11 +87,11 @@ public class UserVO extends Throwable {
 	}
 
 	public String getPw() {
-		return this.pw;
+		return this.password;
 	}
 
 	public void setPw(String pw) {
-		this.pw = pw;
+		this.password = pw;
 	}
 
 	public String getName() {
