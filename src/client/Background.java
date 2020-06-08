66c2d5ce3060be20f2c86fe6f3b51e24c0ed15a9
LeeHyerno <wjstkgusgh18@naver.com>
2020-06-08 15:05:22 +0900

    MainScreen 수정

---
+++ src/client/Background.java
@@ -0,0 +1,20 @@
+package client;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+public class Background extends JPanel {
+	Image image;
+
+	public Background() {
+		image = new ImageIcon(Background.class.getResource("../img/background.jpg")).getImage();
+	}
+
+	public void paintComponent(Graphics g) {
+		g.drawImage(image, 0, 0, null);
+		setOpaque(false);
+
+	}
+
+}
