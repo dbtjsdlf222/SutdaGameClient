d12c6ab27f3ec6d4da2c0b785aebce3a3496c5c3
TJ <TJ@DESKTOP-G4SIDO2>
2020-06-08 14:33:53 +0900

    멀티 채팅 오류 해결

--- src/vo/Protocol.java
+++ src/vo/Protocol.java
@@ -2,11 +2,11 @@ package vo;
 
 public class Protocol {
 	public static final String MESSAGE = "message";
-	public static final String GAMESTART = "gameStart";
 	
 	//Location
 	public static final String Lobby = "Lobby";
 	public static final String Room = "Room";
+	public static final String GAMESTART = "gameStart";
 	
 	//하프 체크 등 추가
 	
