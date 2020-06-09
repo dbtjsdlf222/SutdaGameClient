fca6b98051da376b07744bf31f491af0165ba019
LeeHyerno <wjstkgusgh18@naver.com>
2020-06-09 11:41:03 +0900

    jscrollPane 수정

--- src/client/ReceiveServerPacket.java
+++ src/client/ReceiveServerPacket.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
+import javax.swing.JScrollPane;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import vo.Protocol;
@@ -11,6 +13,7 @@ import vo.Packet;
 
 public class ReceiveServerPacket extends Thread {
 	private Socket socket;
+	public static JScrollPane scrollPane = new JScrollPane(ChattingOperator.chatArea);
 
 	public ReceiveServerPacket(Socket socket) {
 		this.socket = socket;
@@ -18,7 +21,9 @@ public class ReceiveServerPacket extends Thread {
 
 	@Override
 	public void run() {
-		System.out.println("클라이언트 서버 패킷 받기 시작");
+		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane.setBounds(6, 15, 508, 275);
+		
 		ObjectMapper mapper = new ObjectMapper();
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"))) {
 			
@@ -29,7 +34,8 @@ public class ReceiveServerPacket extends Thread {
 				switch (packet.getAction()) {
 					case Protocol.MESSAGE:
 						ChattingOperator.chatArea.append(packet.getPlayerVO().getNic()+": "+ packet.getMotion()+"\n");
-					break;
+						scrollPane.getVerticalScrollBar().setValue(scrollPane.getVerticalScrollBar().getMaximum());
+						break;
 				}
 			}
 		} catch (Exception e) {
