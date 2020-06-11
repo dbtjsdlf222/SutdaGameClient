414ff645c3bcd5b5d052ec0e88d0b9b1fe2080b7
TJ <TJ@DESKTOP-G4SIDO2>
2020-06-11 16:22:32 +0900

    Protocol Room update

--- src/vo/Protocol.java
+++ src/vo/Protocol.java
@@ -3,35 +3,34 @@ package vo;
 public class Protocol {
 	
 	//Server
-	public static final String FIRSTENTER = "0";	//첫 로비 입장
+//	public static final String FIRSTENTER = "0";	//첫 로비 입장
 	public static final String MESSAGE = "1";		//메세지
-	public static final String JOINPLAYER = "2";	//타 플레이어 입실
-	public static final String EXITPLAYER = "3";	//타 플레이어 퇴실
-	public static final String ENTERLOBBY = "4";		//로비입장
-	public static final String MAKEROOM = "5";		//방만들기
-	public static final String ENTERROOM = "10";	//방 입실		입실시 vo에 roomNo 수정
-	public static final String EXITROOM = "11";		//방 퇴실		vo roomNo = 널
-	public static final String EXITLOBBY = "13";		//방 퇴실
-	public static final String LOGIN = "14";
+	public static final String MAKEROOM = "2";		//방만들기
 	
-	public static final String ENTEROTHERROOM = "9";  	//다른 사람이 룸에 입실
-	public static final String ENTEROTHERLOBBY = "15";	//다른 사람이 로비 입실
-	public static final String EXITOTHERROOM = "9";  	//다른 사람이 룸에 퇴실
-	public static final String EXITOTHERLOBBY = "15";	//다른 사람이 로비 퇴실
+	public static final String ENTERROOM = "3";	//방 입실		입실시 vo에 roomNo 수정
+	public static final String EXITROOM = "4";		//방 퇴실		vo roomNo = 널
+	public static final String ENTERLOBBY = "5";		//로비입장
+	public static final String EXITLOBBY = "6";		//방 퇴실
 	
-	//Location
-	public static final String LOBBY = "lobby";
-	public static final String ROOM = "room";
+	public static final String LOGIN = "7";
+	
+	public static final String ENTEROTHERROOM = "8";  	//다른 사람이 룸에 입실
+	public static final String ENTEROTHERLOBBY = "9";	//다른 사람이 로비 입실
+	public static final String EXITOTHERROOM = "10";  	//다른 사람이 룸에 퇴실
+	public static final String EXITOTHERLOBBY = "11";	//다른 사람이 로비 퇴실
 	
 	
 	//Game
-	public static final String GAMESTART = "gameStart";	//game 시작 상태 만들기
-	public static final String Half = "half";
-	public static final String Quater = "quater";
-	public static final String Check = "checkt";
-	public static final String Allin = "allIn";
-	public static final String Call = "call";
-	public static final String Die = "die";
-	public static final String Pping = "pping";
+	public static final String GAMESTART = "12";	//game 시작 상태 만들기
+	public static final String DRAW = "13";	//game 시작 상태 만들기
+	
+	
+	public static final String Half = "14";
+	public static final String Quater = "15";
+	public static final String Check = "16";
+	public static final String Allin = "17";
+	public static final String Call = "18";
+	public static final String Die = "19";
+	public static final String Pping = "20";
 	
 }
