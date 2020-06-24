28526c3875d7461263f115491aede0f86493f6ac
kkkkhyo <66204107+kkkkhyo@users.noreply.github.com>
2020-06-25 00:21:50 +0900

    Jokbo add

--- src/util/Jokbo.java
+++ src/util/Jokbo.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 class Card {
 	private float card1;
 	private float card2;
-	
+
 	public Card(float card1, float card2) {
 		this.card1 = card1;
 		this.card2 = card2;
@@ -17,11 +17,12 @@ public class Jokbo {
 	private ArrayList<Jokbo> jokboList;
 	private String name;
 	private ArrayList<Card> cardComboArr;
-	
+
 	private Jokbo() {
-		//생성자가 실행되면 모든 족보 add
+		// 생성자가 실행되면 모든 족보 add
 		jokboList = new ArrayList<Jokbo>();
 		cardComboArr = new ArrayList<>();
+
 //		jokboList.add(new Jokbo(3,8,"38광땡"));
 //		jokboList.add(new Jokbo(1,8,"18광땡"));
 //		jokboList.add(new Jokbo(1,3,"13광땡"));
@@ -30,61 +31,137 @@ public class Jokbo {
 //			if(i==10)
 //				jokboList.add(new Jokbo(10, (float) 10.5, "장땡"));
 //		}
-		
-		//알리
-		Jokbo a = new Jokbo("알리");
-		a.cardComboArr.add(new Card(1,2));
-		a.cardComboArr.add(new Card(1,2.5f));
-		a.cardComboArr.add(new Card(1.5f,2));
-		a.cardComboArr.add(new Card(1.5f,2.5f));
+
+		// 38광땡
+		Jokbo a = new Jokbo("38광땡");
+		a.cardComboArr.add(new Card(3, 8));
 		jokboList.add(a);
-		//독사
-		Jokbo b = new Jokbo("독사");
-		b.cardComboArr.add(new Card(1,4));
-		b.cardComboArr.add(new Card(1,4.5f));
-		b.cardComboArr.add(new Card(1.5f,4));
-		b.cardComboArr.add(new Card(1.5f,4.5f));
+		// 18광땡
+		Jokbo b = new Jokbo("18광땡");
+		b.cardComboArr.add(new Card(1, 8));
 		jokboList.add(b);
-		//구삥
-		Jokbo c = new Jokbo("구삥");
-		c.cardComboArr.add(new Card(1,9));
-		c.cardComboArr.add(new Card(1,9.5f));
-		c.cardComboArr.add(new Card(1.5f,9));
-		c.cardComboArr.add(new Card(1.5f,9.5f));
+		// 13광땡
+		Jokbo c = new Jokbo("13광땡");
+		c.cardComboArr.add(new Card(1, 3));
 		jokboList.add(c);
-		//장삥
-		Jokbo d = new Jokbo("장삥");
-		d.cardComboArr.add(new Card(1,10));
-		d.cardComboArr.add(new Card(1,10.5f));
-		d.cardComboArr.add(new Card(1.5f,10));
-		d.cardComboArr.add(new Card(1.5f,10.5f));
-		jokboList.add(d);
-		//장사
-		Jokbo e = new Jokbo("장사");
-		e.cardComboArr.add(new Card(4,10));
-		e.cardComboArr.add(new Card(4,10.5f));
-		e.cardComboArr.add(new Card(4.5f,10));
-		e.cardComboArr.add(new Card(4.5f,10.5f));
+		// 땡
+		Jokbo da = new Jokbo("장땡");
+		da.cardComboArr.add(new Card(10,10.5f));
+		jokboList.add(da);
+		Jokbo db = new Jokbo("9땡");
+		db.cardComboArr.add(new Card(9,9.5f));
+		jokboList.add(db);
+		Jokbo dc = new Jokbo("8땡");
+		dc.cardComboArr.add(new Card(8,8.5f));
+		jokboList.add(dc);
+		Jokbo dd = new Jokbo("7땡");
+		dd.cardComboArr.add(new Card(7,7.5f));
+		jokboList.add(dd);
+		Jokbo de = new Jokbo("6땡");
+		de.cardComboArr.add(new Card(6,6.5f));
+		jokboList.add(de);
+		Jokbo df = new Jokbo("5땡");
+		df.cardComboArr.add(new Card(5,5.5f));
+		jokboList.add(df);
+		Jokbo dg = new Jokbo("4땡");
+		dg.cardComboArr.add(new Card(4,4.5f));
+		jokboList.add(dg);
+		Jokbo dh = new Jokbo("3땡");
+		dh.cardComboArr.add(new Card(3,3.5f));
+		jokboList.add(dh);
+		Jokbo di = new Jokbo("2땡");
+		di.cardComboArr.add(new Card(2,2.5f));
+		jokboList.add(di);
+		Jokbo dj = new Jokbo("1땡");
+		dj.cardComboArr.add(new Card(1,1.5f));
+		jokboList.add(dj);
+		// 알리
+		Jokbo e = new Jokbo("알리");
+		e.cardComboArr.add(new Card(1, 2));
+		e.cardComboArr.add(new Card(1, 2.5f));
+		e.cardComboArr.add(new Card(1.5f, 2));
+		e.cardComboArr.add(new Card(1.5f, 2.5f));
 		jokboList.add(e);
-		//세륙
-		Jokbo f = new Jokbo("세륙");
-		f.cardComboArr.add(new Card(4,6));
-		f.cardComboArr.add(new Card(4,6.5f));
-		f.cardComboArr.add(new Card(4.5f,6));
-		f.cardComboArr.add(new Card(4.5f,6.5f));
+		// 독사
+		Jokbo f = new Jokbo("독사");
+		f.cardComboArr.add(new Card(1, 4));
+		f.cardComboArr.add(new Card(1, 4.5f));
+		f.cardComboArr.add(new Card(1.5f, 4));
+		f.cardComboArr.add(new Card(1.5f, 4.5f));
 		jokboList.add(f);
-		
+		// 구삥
+		Jokbo g = new Jokbo("구삥");
+		g.cardComboArr.add(new Card(1, 9));
+		g.cardComboArr.add(new Card(1, 9.5f));
+		g.cardComboArr.add(new Card(1.5f, 9));
+		g.cardComboArr.add(new Card(1.5f, 9.5f));
+		jokboList.add(g);
+		// 장삥
+		Jokbo h = new Jokbo("장삥");
+		h.cardComboArr.add(new Card(1, 10));
+		h.cardComboArr.add(new Card(1, 10.5f));
+		h.cardComboArr.add(new Card(1.5f, 10));
+		h.cardComboArr.add(new Card(1.5f, 10.5f));
+		jokboList.add(h);
+		// 장사
+		Jokbo i = new Jokbo("장사");
+		i.cardComboArr.add(new Card(4, 10));
+		i.cardComboArr.add(new Card(4, 10.5f));
+		i.cardComboArr.add(new Card(4.5f, 10));
+		i.cardComboArr.add(new Card(4.5f, 10.5f));
+		jokboList.add(i);
+		// 세륙
+		Jokbo j = new Jokbo("세륙");
+		j.cardComboArr.add(new Card(4, 6));
+		j.cardComboArr.add(new Card(4, 6.5f));
+		j.cardComboArr.add(new Card(4.5f, 6));
+		j.cardComboArr.add(new Card(4.5f, 6.5f));
+		jokboList.add(j);
+		// 땡잡이
+		Jokbo k = new Jokbo("땡잡이");
+		k.cardComboArr.add(new Card(3, 7));
+		jokboList.add(k);
+		// 암행어사
+		Jokbo l = new Jokbo("암행어사");
+		l.cardComboArr.add(new Card(4, 7));
+		jokboList.add(l);
+		// 구사 재경기
+		Jokbo m = new Jokbo("구사 재경기");
+		m.cardComboArr.add(new Card(4, 9.5f));
+		m.cardComboArr.add(new Card(4.5f, 9));
+		m.cardComboArr.add(new Card(4.5f, 9.5f));
+		jokboList.add(m);
+		// 멍텅구리 구사
+		Jokbo n = new Jokbo("멍텅구리 구사");
+		n.cardComboArr.add(new Card(4,9));
+		jokboList.add(n);
 	}
 
 	public static Jokbo getInstance() {
-		if(instance == null) { instance = new Jokbo(); }
+		if (instance == null) {
+			instance = new Jokbo();
+		}
 		return instance;
 	}
-	
-	public ArrayList<Jokbo> getJokboList() { return jokboList; }
-	public Jokbo(String name) { this.name = name; }
-	public void setJokboList(ArrayList<Jokbo> jokboList) { this.jokboList = jokboList; }
-	public String getName() { return name; }
-	public void setName(String name) { this.name = name; }
-	
-} //class
+
+	public ArrayList<Jokbo> getJokboList() {
+		return jokboList;
+	}
+
+	public Jokbo(String name) {
+		this.name = name;
+	}
+
+	public void setJokboList(ArrayList<Jokbo> jokboList) {
+		this.jokboList = jokboList;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+} // class
