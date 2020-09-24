aa7c96e93be48a69f93660608f47a3bb17ee3dfe
유선일 <34783191+dbtjsdlf222@users.noreply.github.com>
2020-09-25 00:44:54 +0900

    돈 오류 수정

--- src/util/MoneyFormat.java
+++ src/util/MoneyFormat.java
@@ -4,35 +4,35 @@ import java.text.DecimalFormat;
 import java.util.regex.Pattern;
 
 public class MoneyFormat {
-	
-	public synchronized static String format(long money) {
-		System.out.println("formar "+money);
-        DecimalFormat d = new DecimalFormat("#,####");
-       
-        String[] unit = new String[]{"만","억","조","경"};
-        String[] str = d.format(money).split(",");
-        String result = "";
-        int cnt = 0;
-        for(int i=str.length;i>0;i--) {
-            if(Integer.parseInt(str[i-1]) != 0) {
-                result = String.valueOf(Integer.parseInt(str[i-1])) + unit[cnt] + result;
-            }
-            cnt++;
-        }
-        System.out.println("result "+result);
-        return result; 
-    }
-	
-	public synchronized static String format(String money) {
+
+	public static String format(long money) {
+		System.out.println("formar " + money);
+		DecimalFormat d = new DecimalFormat("#,####");
+
+		String[] unit = new String[] { "만", "억", "조", "경" };
+		String[] str = d.format(money).split(",");
+		String result = "";
+		int cnt = 0;
+		for (int i = str.length; i > 0; i--) {
+			if (Integer.parseInt(str[i - 1]) != 0) {
+				result = String.valueOf(Integer.parseInt(str[i - 1])) + unit[cnt] + result;
+			}
+			cnt++;
+		} //for
+		System.out.println("result " + result);
+		return result;
+	}
+
+	public static String format(String money) {
 		return format(Long.parseLong(money));
 	}
-	
-	public synchronized static Long reformat(String money) {
+
+	public static Long reformat(String money) {
 		System.out.println("reformat "+money);
 		String[] unit = new String[]{"만","억","조","경"};
-		String[] sp = money.split("");
-		long maxUnit=0;
-		long unitCnt=0;
+		String[] sArr = money.split("");
+		int maxUnit=0;
+		int unitCnt=0;
 		long total=0;
 		for (int i = 3; i >= 0; i--) {
 			if(money.indexOf(unit[i]) != -1) {
@@ -42,31 +42,35 @@ public class MoneyFormat {
 			}
 		} //for
 		String temp = "";
+		
 		for (int i = 0; i < money.length(); i++) {
-			if(Pattern.matches("[0-9]",sp[i])) {
-				temp = temp + sp[i];
+			if(Pattern.matches("[0-9]",sArr[i])) {
+				temp = temp + sArr[i];
 			} else {
-					unitCnt-=1;
-					if(temp!="") {
-						if(unitCnt==3) {
-							total+=(Long.parseLong(temp)*1000000000000L);		
-						} else if(unitCnt==2) {
-							total+=(Long.parseLong(temp)*100000000L);
-						} else if(unitCnt==1){
-							total+=(Long.parseLong(temp)*10000L);
-						} else {
-							total+=(Long.parseLong(temp)*1L);
-						}
+				unitCnt-=1;
+				if(temp!="") {
+					while(!sArr[i].equals(unit[unitCnt]))
+						unitCnt--;
+					
+					if(unitCnt==3) {
+						total+=(Long.parseLong(temp)*1000000000000L);		
+					} else if(unitCnt==2) {
+						total+=(Long.parseLong(temp)*100000000L);
+					} else if(unitCnt==1){
+						total+=(Long.parseLong(temp)*10000L);
+					} else {
+						total+=(Long.parseLong(temp)*1L);
 					}
+				}
 				temp="";
 			} //else
 		}
 		System.out.println("result "+total);
 		return total;
-	} //reformat();
-	
+	} // reformat();
+
 	public static void main(String[] args) {
-		reformat(format(18430000000L));
+		reformat(format(1000084307000L));
 	}
-	
-} //MoneyFormat
+
+} // MoneyFormat
