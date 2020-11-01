b33dd35a7aeb9666b4c56e192f4f5a9b7a20e55c
유선일 <34783191+dbtjsdlf222@users.noreply.github.com>
2020-11-01 22:18:15 +0900

    필요없는거 제거

--- src/server/Room.java
+++ src/server/Room.java
@@ -593,9 +593,6 @@ public class Room extends ServerMethod {
 		} else {
 			winMsg = playerMap.get(winerIdx).getNic()+"님이 "+cardName+"(으)로 승입니다. "+"/";
 		}
-		System.out.println("토탈 " + totalMoney);
-		System.out.println("승리자머니 " + playerMap.get(winerIdx).getMoney());
-		System.out.println("토탈 +승리자머니 " + (totalMoney+playerMap.get(winerIdx).getMoney()));
 		roomSpeaker(new Packet(Protocol.GAME_OVER,
 					winMsg+
 					winerIdx+"/" +
